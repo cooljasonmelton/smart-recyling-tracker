bca356224f66b93a465b78be73de08a9bb1d7596
Jason <jason.melton2@gmail.com>
2025-08-20 04:57:14 -0500

    set up fe

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>smart recycling tracker</h1>
+      <h2>user info:</h2>
+      <h2>recycling info:</h2>
+    </div>
+  );
+}
+
+export default App;
